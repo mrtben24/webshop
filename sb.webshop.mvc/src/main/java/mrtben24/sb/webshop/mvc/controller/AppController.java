5454a15724e02227a8c2e14c3b5e864380f43eed
mrtben24 <tothbence24@gmail.com>
2023-05-23 17:28:12 +0200

    dev

--- sb.webshop.mvc/src/main/java/mrtben24/sb/webshop/mvc/controller/AppController.java
+++ sb.webshop.mvc/src/main/java/mrtben24/sb/webshop/mvc/controller/AppController.java
@@ -3,13 +3,18 @@ package mrtben24.sb.webshop.mvc.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import mrtben24.sb.webshop.mvc.db.Database;
+import mrtben24.sb.webshop.mvc.model.Product;
 
 public class AppController {
 
 	@GetMapping("/")
 	String showIndex() {
 		
-		
+		Database db = new Database();
+		Product product = new Product();
+		product = db.getAllProduct(1);
+		System.out.println("defwef");
+		System.out.println(product);
 		
 		return "index.html";
 	}
--- sb.webshop.mvc/src/main/java/mrtben24/sb/webshop/mvc/db/Database.java
+++ sb.webshop.mvc/src/main/java/mrtben24/sb/webshop/mvc/db/Database.java
@@ -1,9 +1,16 @@
 package mrtben24.sb.webshop.mvc.db;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import mrtben24.sb.webshop.mvc.model.Product;
 
 public class Database {
 
@@ -18,7 +25,20 @@ public class Database {
 		sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 	}
 
-	
+	public Product getAllProduct(int productId) {
+		
+		Product product = null;
+		
+		Session session = sessionFactory.openSession();
+		Transaction tr = session.beginTransaction();
+		
+		product = session.get(Product.class, productId);
+		
+		tr.commit();
+		session.close();
+		
+		return product;
+	}
 	
 	public void closeDb() {
 		
