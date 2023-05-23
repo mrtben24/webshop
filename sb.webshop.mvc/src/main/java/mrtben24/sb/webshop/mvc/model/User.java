0706a1baf4c8f3ee21b57504c6ad86b0947713a3
mrtben24 <tothbence24@gmail.com>
2023-05-23 16:37:46 +0200

    model classes done

--- sb.webshop.mvc/src/main/java/mrtben24/sb/webshop/mvc/model/User.java
+++ sb.webshop.mvc/src/main/java/mrtben24/sb/webshop/mvc/model/User.java
@@ -1,5 +1,64 @@
 package mrtben24.sb.webshop.mvc.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	int id;
+	
+	@Column(name="name")
+	String name;
+	
+	@Transient
+	List<Integer> userProdIdList;
+
+	public User() {
+		
+		this.userProdIdList = new ArrayList<>();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Integer> getUserProdIdList() {
+		return userProdIdList;
+	}
+
+	public void setUserProdIdList(List<Integer> userProdIdList) {
+		this.userProdIdList = userProdIdList;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", userProdIdList=" + userProdIdList + "]";
+	}
+	
+	
+	
 }
